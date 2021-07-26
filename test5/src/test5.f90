fbdcdb61fa9569935d9b3685d165b4dfe01e56fa
Miro Mocak <34376626+mmicromegas@users.noreply.github.com>
2021-07-26 10:17:43 +0200

    initial commit

---
+++ test5/src/test5.f90
@@ -0,0 +1,12 @@
+! ============================================================================
+! Name        : test5.f90
+! Author      : 
+! Version     :
+! Copyright   : Your copyright notice
+! Description : Hello World in Fortran
+! ============================================================================
+
+program test5
+    implicit none
+    print *, "Hello MPI World 2dsfasdfas"
+end program
